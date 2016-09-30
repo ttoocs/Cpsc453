a8fb359141962f3e5b4a550a2c31ef2d3904098d
ttoocs <ttoocs@gmail.com>
2016-09-30 03:28:17 -0600

    Something about casting failures

--- A1/noboiler/main.cpp
+++ A1/noboiler/main.cpp
@@ -8,6 +8,8 @@
 #include <iterator>
 #include <algorithm>
 
+#include <unistd.h>
+
 #define GLFW_INCLUDE_GLCOREARB
 #define GL_GLEXT_PROTOTYPES
 #include <GLFW/glfw3.h>
@@ -21,17 +23,28 @@ using namespace std;
 void QueryGLVersion();
 bool CheckGLErrors();
 
-//Initalizes various things shared between scenes.
-bool initGL(){
+//Declaring functions defined later.
+string LoadSource(const string &filename);
 
+
+//Initalizes various things shared between scenes.
+bool initGL(GLuint * verBuff, GLuint * colBuff){
+
+	
 	//Vertex Shader
+	string vertexStr = LoadSource("vertex.glsl");
+	const GLchar *vertexSource = vertexStr.c_str();
+	string fragmentStr = LoadSource("vertex.glsl");
+	const GLchar *fragmentSource = fragmentStr.c_str();
+
+
 	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, "vertex.glsl", NULL);
+	glShaderSource(vertexShader, 1, &vertexSource, 0);
 	glCompileShader(vertexShader);
 
 	//Fragment Shader
 	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, "fragment.glsl", NULL);
+	glShaderSource(fragmentShader, 1, &fragmentSource, 0);
 	glCompileShader(fragmentShader);
 
 	GLuint shaderProgram = glCreateProgram();
@@ -39,9 +52,21 @@ bool initGL(){
 	glAttachShader(shaderProgram, fragmentShader);
 
 	glLinkProgram(shaderProgram);
+	GLint link_ok;
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &link_ok);
 	glUseProgram(shaderProgram);	
 
-	return !CheckGLErrors();
+	if(!link_ok){
+		return 1; //Just assume everything happend alright..
+	}
+
+	glGenBuffers(1,verBuff);
+	glBindBuffer(GL_ARRAY_BUFFER,*verBuff);	//Init glBuffer for verticies
+
+	glGenBuffers(1,colBuff);
+	glBindBuffer(GL_ARRAY_BUFFER,*colBuff);	//Init glBuffer for colours.
+	
+	glClearColor(0,0,0,0);
 }
 
 void end_prog(int code){
@@ -58,6 +83,38 @@ void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods
 }
 
 
+void Render(GLuint *vertexBuffer, GLuint * colourBuffer, GLfloat * verts[], GLfloat * colours[], int start, int stop){
+	//Configure the buffers to use the arrays.
+	glBufferData(GL_ARRAY_BUFFER, sizeof(*verts), *verts, GL_STATIC_DRAW); //Verts.
+	glBufferData(GL_ARRAY_BUFFER, sizeof(*colours), *colours, GL_STATIC_DRAW); //Colours.
+	//TODO: Delete these when done.	
+
+    const GLuint VERTEX_INDEX = 0; //Apprently not constants. TODO: Figure out what is actually going on here.
+    const GLuint COLOUR_INDEX = 1;
+
+	glBindBuffer(GL_ARRAY_BUFFER, *vertexBuffer);
+	glVertexAttribPointer(VERTEX_INDEX, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(VERTEX_INDEX);
+
+	glBindBuffer(GL_ARRAY_BUFFER, *colourBuffer);		
+	glVertexAttribPointer(COLOUR_INDEX, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(COLOUR_INDEX);
+
+	
+	glClear(GL_COLOR_BUFFER_BIT);
+	glDrawArrays(GL_LINES, start, stop);
+	
+}
+
+void test(GLuint *vertexBuffer, GLuint * colourBuffer){
+	GLfloat verts[][2] = {
+		{-1,0},{1,0}
+	};
+	GLfloat colours[][3] = {
+		{1,1,1}
+	};
+	Render(vertexBuffer, colourBuffer, &verts, &colours);
+}
 
 int main(int argc, char *argv[]){
 	if(!glfwInit()){
@@ -66,9 +123,9 @@ int main(int argc, char *argv[]){
 	}
 
 	
-	GLFWwindow *window 0;
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, GLMAJOR_VER);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, GLMINOR_VER);
+	GLFWwindow *window = 0;
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);		//Setup the window, with things.
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     window = glfwCreateWindow(512, 512, "CPSC 453 Assignment 1", 0, 0);
@@ -82,15 +139,38 @@ int main(int argc, char *argv[]){
     glfwSetKeyCallback(window, KeyCallback);
     glfwMakeContextCurrent(window);
 
-	if(!initGL()){
+	GLuint *vertexBuffer;
+	GLuint *colourBuffer;
+	if(!initGL(vertexBuffer,colourBuffer)){
 		cout << "Initalizing GL failed" << endl;
 		end_prog(-3);
 	}
 
+	test(vertexBuffer, colourBuffer);	
 	
+	sleep(32);	
 }
 
 
 
-
+//Taken verbatim from boilerplate.cpp
+// reads a text file with the given name into a string
+string LoadSource(const string &filename)
+{   
+    string source;
+    
+    ifstream input(filename.c_str());
+    if (input) {
+        copy(istreambuf_iterator<char>(input),
+             istreambuf_iterator<char>(),
+             back_inserter(source));
+        input.close();
+    }
+    else {
+        cout << "ERROR: Could not load shader source from file "
+             << filename << endl;
+    }
+    
+    return source;
+}
 
